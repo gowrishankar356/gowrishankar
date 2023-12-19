fe6268f7ce39d69dac36598ab5cd4ffca938f307
Badugu Gowri Shankar <gowrishankar356@gmail.com>
2023-12-19 15:14:20 +0530

    new deployments

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import Skills from "./pages/Skills";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/gowrishankar">
         <Routes>
-          <Route path="/"  element={<HomePage />} />
-          <Route path="/skills"  element={<Skills />} />
-          <Route path="/projects"  element={<Projects />} />
-          <Route path="/experience"  element={<WorkExperience />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<WorkExperience />} />
         </Routes>
       </BrowserRouter>
     </div>
